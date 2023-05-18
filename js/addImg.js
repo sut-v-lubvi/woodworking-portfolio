7ac3addcfa31a488afdc15ed80594b57173934dc
cipher <sut-v-lubvi@yandex.ru>
2023-05-18 18:31:43 +0300

    goPhoto

--- js/addImg.js
+++ js/addImg.js
@@ -1,3 +1,10 @@
+//find all links and pictures
+var picList = document.querySelectorAll('.picture');
+var picturesList = Array.from(picList);
+var liList = document.querySelectorAll('.linc');
+//convert nodeList to array
+var lincList = Array.from(liList);
+//change burger style on click
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 let headerShadow = document.getElementById('header');
@@ -6,118 +13,77 @@ menuBtn.addEventListener('click', function () {
 	menu.classList.toggle('active');
 	headerShadow.classList.toggle('header__shadow')
 })
-
-var pictureOne = document.getElementById('picture1');
-var pictureToo = document.getElementById('picture2');
-var pictureTree = document.getElementById('picture3');
-var pictureFour = document.getElementById('picture4');
-var pictureFive = document.getElementById('picture5');
-var pictureSix = document.getElementById('picture6');
-
-//Нахожу все ссылки и картинки
-var lincStairs = document.getElementById('lincStairs');
-var lincDiningRoom = document.getElementById('lincDiningRoom');
-var lincLibrary = document.getElementById('lincLibrary');
-var lincBatroom = document.getElementById('lincBatroom');
-var lincСhildren = document.getElementById('lincСhildren');
-var lincWardrobe = document.getElementById('lincWardrobe');
-var lincСloset = document.getElementById('lincСloset');
-var lincDoor = document.getElementById('lincDoor');
-
-const lincsEl = document.querySelectorAll('.linc');
-console.log(lincsEl);
-lincsEl.forEach(lincsEl => {
+lincList.forEach(lincsEl => {
 	lincsEl.addEventListener('click', function(){
 		menuBtn.classList.toggle('active');
 		menu.classList.toggle('active');
 		headerShadow.classList.toggle('header__shadow')
 	})
 })
-//Добавляю onclick ко всем ссылкам-категориям
-//var arrayLinc = [lincStairs,lincDiningRoom,lincLibrary,lincBatroom,lincСhildren,lincWardrobe,lincСloset,lincDoor];
-//function onclickIinc(i) {
-	//arrayLinc[i].onclick = goPhoto;
-//}
-//for (var i = 0; i < arrayLinc.length; i++) {
-	//onclickIinc(i);
-//}
+//create arrays with src images
+var photoArray1 = ['/img/1stairs/1.jpg','/img/1stairs/2.jpg','/img/1stairs/3.jpg','/img/1stairs/4.jpg','/img/1stairs/5.jpg','/img/1stairs/6.jpg'];
+var photoArray2 = ['/img/2diningRoom/1.jpg','/img/2diningRoom/2.jpg','/img/2diningRoom/3.jpg','/img/2diningRoom/4.jpg','/img/2diningRoom/5.jpg','/img/2diningRoom/6.jpg'];
+var photoArray3 = ['/img/3library/1.jpg','/img/3library/2.jpg','/img/3library/3.jpg','/img/3library/4.jpg','/img/3library/5.jpg','/img/3library/6.jpg'];
+var photoArray4 = ['/img/4batroom/1.jpg','/img/4batroom/2.jpg','/img/4batroom/3.jpg','/img/4batroom/4.jpg','/img/4batroom/5.png','/img/4batroom/6.jpg'];
+var photoArray5 = ['/img/5children_room/1.jpg','/img/5children_room/2.jpg','/img/5children_room/3.jpg','/img/5children_room/4.jpg','/img/5children_room/5.jpeg','/img/5children_room/6.jpeg'];
+var photoArray6 = ['/img/6wardrobe/1.jpg','/img/6wardrobe/2.jpg','/img/6wardrobe/3.jpg','/img/6wardrobe/4.jpg','/img/6wardrobe/5.jpg','/img/6wardrobe/6.jpg'];
+var photoArray7 = ['/img/7closet/1.jpg','/img/7closet/2.jpg','/img/7closet/3.jpg','/img/7closet/4.jpg','/img/7closet/5.jpg','/img/7closet/6.jpg'];
+var photoArray8 = ['/img/8door/1.jpg','/img/8door/2.jpg','/img/8door/3.jpg','/img/8door/4.jpg','/img/8door/5.jpg','/img/8door/6.jpg'];
+let photoArray = [photoArray1,photoArray2,photoArray3,photoArray4,photoArray5,photoArray6,photoArray7,photoArray8];
+var LincId;
+// assign addEventListener to all links
+var j = 0;
+for (;j < lincList.length; j++) {
+	LincId = lincList[j].id;
+	lincList[j].addEventListener('click',	goPhoto(LincId));
+}
+// function goListenerInLinc() {
+// 	LincId = lincList[j].id;
+// 	lincList[j].addEventListener('click',	goPhoto(LincId));
+// 	j++;
+// 	if (j == 8) return
+// 	goListenerInLinc();
+// }
+// goListenerInLinc();
 
-//var arrayphotoUrl = ['/img/stairs/1.jpg','/img/stairs/02.jpg','/img/stairs/03.jpg','/img/stairs/04.jpg','/img/stairs/05.jpg','/img/stairs/06.jpg',];
-//var arrayPicrures = [pictureOne,pictureToo,pictureTree,pictureFour,pictureFive,pictureSix];
-//for (var i = 0; i < arrayPicrures.length; i++) {
-//	goPhoto(i);
-//}
-lincStairs.onclick = goPhotoStairs;
-lincDiningRoom.onclick = goPhotoDiningRoom;
-lincLibrary.onclick = goPhotoLibrary;
-lincBatroom.onclick = goPhotoBatroom;
-lincСhildren.onclick = goPhotoСhildren;
-lincWardrobe.onclick = goPhotoWardrobe;
-lincСloset.onclick = goPhotoСloset;
-lincDoor.onclick = goPhotoDoor;
-goPhotoStairs();
-function goPhotoStairs() {
-	pictureOne.src = '/img/1stairs/1.jpg';
-	pictureToo.src = '/img/1stairs/2.jpg';
-	pictureTree.src = '/img/1stairs/3.jpg';
-	pictureFour.src = '/img/1stairs/4.jpg';
-	pictureFive.src = '/img/1stairs/5.jpg';
-	pictureSix.src = '/img/1stairs/6.jpg';
+//creating a photo upload function
+let i = 0;
+function goPhoto(LincId) {
+	return function() {
+		if (LincId === 'lincStairs') {
+			goPhotoGoSrc(photoArray1);
+			}else if (LincId === 'lincDiningRoom') {
+			goPhotoGoSrc(photoArray2);
+			}else if (LincId === 'lincLibrary') {
+				goPhotoGoSrc(photoArray3);
+			}else if (LincId === 'lincBatroom') {
+				goPhotoGoSrc(photoArray4);
+			}else if (LincId === 'lincСhildren') {
+				goPhotoGoSrc(photoArray5);
+			}else if (LincId === 'lincWardrobe') {
+				goPhotoGoSrc(photoArray6);
+			}else if (LincId === 'lincСloset') {
+				goPhotoGoSrc(photoArray7);
+			}else if (LincId === 'lincDoor') {
+				goPhotoGoSrc(photoArray8);
+			}
+			}	
+	}
+
+function goPhotoGoSrc(params) {
+	for (let index = 0; index < picturesList.length; index++) {
+		picturesList[index].src = params[i];
+		i++;
+		}	
 }
-function goPhotoDiningRoom() {
-	pictureOne.src = '/img/2diningRoom/1.jpg';
-	pictureToo.src = '/img/2diningRoom/2.jpg';
-	pictureTree.src = '/img/2diningRoom/3.jpg';
-	pictureFour.src = '/img/2diningRoom/4.jpg';
-	pictureFive.src = '/img/2diningRoom/5.jpg';
-	pictureSix.src = '/img/2diningRoom/6.jpg';
-}
-function goPhotoLibrary() {
-	pictureOne.src = '/img/3library/1.jpg';
-	pictureToo.src = '/img/3library/2.jpg';
-	pictureTree.src = '/img/3library/3.jpg';
-	pictureFour.src = '/img/3library/4.jpg';
-	pictureFive.src = '/img/3library/5.jpg';
-	pictureSix.src = '/img/3library/6.jpg';
-}
-function goPhotoBatroom() {
-	pictureOne.src = '/img/4batroom/1.jpg';
-	pictureToo.src = '/img/4batroom/2.jpg';
-	pictureTree.src = '/img/4batroom/3.jpg';
-	pictureFour.src = '/img/4batroom/4.jpg';
-	pictureFive.src = '/img/4batroom/5.png';
-	pictureSix.src = '/img/4batroom/6.jpg';
-}
-function goPhotoСhildren() {
-	pictureOne.src = '/img/5children_room/1.jpg';
-	pictureToo.src = '/img/5children_room/2.jpg';
-	pictureTree.src = '/img/5children_room/3.jpg';
-	pictureFour.src = '/img/5children_room/4.jpg';
-	pictureFive.src = '/img/5children_room/5.jpeg';
-	pictureSix.src = '/img/5children_room/6.jpeg';
-}
-function goPhotoWardrobe() {
-	pictureOne.src = '/img/6wardrobe/1.jpg';
-	pictureToo.src = '/img/6wardrobe/2.jpg';
-	pictureTree.src = '/img/6wardrobe/3.jpg';
-	pictureFour.src = '/img/6wardrobe/4.jpg';
-	pictureFive.src = '/img/6wardrobe/5.jpg';
-	pictureSix.src = '/img/6wardrobe/6.jpg';
-}
-function goPhotoСloset() {
-	pictureOne.src = '/img/7closet/1.jpg';
-	pictureToo.src = '/img/7closet/2.jpg';
-	pictureTree.src = '/img/7closet/3.jpg';
-	pictureFour.src = '/img/7closet/4.jpg';
-	pictureFive.src = '/img/7closet/5.jpg';
-	pictureSix.src = '/img/7closet/6.jpg';
-}
-function goPhotoDoor() {
-	pictureOne.src = '/img/8door/1.jpg';
-	pictureToo.src = '/img/8door/2.jpg';
-	pictureTree.src = '/img/8door/3.jpg';
-	pictureFour.src = '/img/8door/4.jpg';
-	pictureFive.src = '/img/8door/5.jpg';
-	pictureSix.src = '/img/8door/6.jpg';
-}
+// }
+// function goPhotoStairs() {
+// 	picturesList[0].src = '/img/1stairs/1.jpg';
+// 	picturesList[1].src = '/img/1stairs/2.jpg';
+// 	picturesList[2].src = '/img/1stairs/3.jpg';
+// 	picturesList[3].src = '/img/1stairs/4.jpg';
+// 	picturesList[4].src = '/img/1stairs/5.jpg';
+// 	picturesList[5].src = '/img/1stairs/6.jpg';
+// }
+
 
